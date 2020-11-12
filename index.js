0222d0d620f630766d3d59d8368bfdd2bd55efec
David <chen7david@me.com>
2020-11-12 22:32:27 +0800

    update

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ class Hotfile {
         this.isDirectory ? this.children = [] : this.ext = p.extname(path)
     }
 
+    filename(name, ext){
+        if(ext) this.ext = ext
+        return [
+            name || this.name, 
+            this.ext && this.ext.replace('.','') || null
+        ].filter(e => e != null).join('.')
+    }
+
     static async map(path, options = {}){
         const items = await this.readdir(path, options)
         for(let i = 0; i < items.length; i++){
@@ -38,6 +46,59 @@ class Hotfile {
     async exists(file) {
         return fs.promises.access(file, fs.constants.F_OK)
             .then(() => true).catch(() => false)
+    }
+
+    async rename(name, ext){
+        if(ext) this.ext = ext
+        const toPath = p.join(this.parent, this.filename(name))
+        return await this.move(toPath)
+    }
+
+    async move(toPath){
+        return fs.promises.rename(this.path, toPath).then(async () => {
+                await Object.assign(this, new Hotfile(toPath))
+                return true
+        }).catch((err) => {
+            console.log(err)
+            return false
+        })
+    }
+
+    async createChildDir(name){
+        if(this.isFile) throw('append only works on directories')
+        let path = p.join(this.path, name) 
+        return fs.promises.mkdir(path, { recursive: true })
+            .then(() => {
+                const dir = new Hotfile(path)
+                this.children.push(dir)
+                return dir
+            }).catch(() => false)
+    }
+
+    async moveTo(toPath){
+        if(toPath instanceof Hotfile) {
+            if(!toPath.isDirectory) throw('you can not move to a file, please provide a directory path')
+            toPath = toPath.path
+        }
+        if(!p.extname(toPath)) toPath = p.join(toPath, this.filename())
+        return fs.promises.rename(this.path, toPath).then(async () => {
+                await Object.assign(this, new Hotfile(toPath))
+                return true
+        }).catch(() => false)
+    }
+
+    filesThrough(item, func){
+        if(!(toPath instanceof Hotfile)) throw('filesThrough expects only instances Hotfiles.')
+        const files = []
+        function extract(item){
+            if(item.isDirectory) {
+                for(let child of item.children) extract(child)
+            }else{
+                func ? files.push(func(item)) : files.push(item)
+            }
+        }
+        extract(item, func)
+        return files
     }
 }
 
