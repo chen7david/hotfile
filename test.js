050089611bd5defbfa64378ebaf6ed9739c69f72
chen7david <chen7david@me.com>
2021-07-03 09:51:56 +0800

    update

--- test.js
+++ test.js
@@ -3,15 +3,16 @@ const { Hotfolder, Hotfile, Hot } = require('./index')
 // const myfolder = require('hotfile')('./myfolder')
 const dirpath = './myfolder'
 const filepath = './myfile.txt'
+if(!Hot.exists(dirpath)) Hot.mkdir(dirpath)
 const myfolder = new Hotfolder(dirpath)
-const myfile = new Hotfile(filepath) 
+// const myfile = new Hotfile(filepath) 
 
 const someAsyncFucn = async () => {
     // create sub-folders
     const subfolder = await myfolder.createFolder('subfolder-05')
     // const subfolder2 = await subfolder.createFolder('subfolder-04')
     const file = await subfolder.create('summer.js')
-    await file.setNameTo('water').moveTo(subfolder)
+    // await file.setNameTo('water').moveTo(subfolder)
 }
 
 someAsyncFucn()
